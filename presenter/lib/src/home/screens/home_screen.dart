26379571d10b5a250aab80c2e0e3548ed26243cd
JHenriqueDev <jhenriquess26@gmail.com>
2022-06-13 10:39:30 -0300

    melhoria na estrututra da home screen

--- presenter/lib/src/home/screens/home_screen.dart
+++ presenter/lib/src/home/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../dependencies/dependencies.dart';
 import '../../src.dart';
 import '../stores/stores.dart';
+import '../components/components.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -37,51 +38,6 @@ class _HomeScreenState extends State<HomeScreen> {
       const ConfigsScreen()
     ];
 
-    final items = [
-      BottomNavigationBarItem(
-          icon: IconsPallete.speedometer.icon,
-          label: L10N(context).text(_screen)['navigation'][0]['label']),
-      BottomNavigationBarItem(
-          icon: IconsPallete.history.icon,
-          label: L10N(context).text(_screen)['navigation'][1]['label']),
-      BottomNavigationBarItem(
-          icon: IconsPallete.configs.icon,
-          label: L10N(context).text(_screen)['navigation'][2]['label']),
-    ];
-
-    final bottomNavigationBar = TripleBuilder(
-      store: store.bottomBarStore,
-      builder: (_, triple) {
-        return BottomNavigationBar(
-          items: items,
-          currentIndex: store.bottomBarStore.state,
-          enableFeedback: true,
-          onTap: (index) {
-            switch (index) {
-              case 0:
-                if (store.bottomBarStore.state == 0) break;
-                store.bottomBarStore.changeIndex(index);
-                store.screenTitleStore
-                    .changeTitle(L10N(context).text('speedometer')['name']);
-                break;
-              case 1:
-                if (store.bottomBarStore.state == 1) break;
-                store.bottomBarStore.changeIndex(index);
-                store.screenTitleStore
-                    .changeTitle(L10N(context).text('history')['name']);
-                break;
-              case 2:
-                if (store.bottomBarStore.state == 2) break;
-                store.bottomBarStore.changeIndex(index);
-                store.screenTitleStore
-                    .changeTitle(L10N(context).text('configs')['name']);
-                break;
-            }
-          },
-        );
-      },
-    );
-
     return DefaultScreen(
       name: TripleBuilder(
         store: store.screenTitleStore,
@@ -89,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
           return Text(store.screenTitleStore.state);
         },
       ),
-      bottomNavigationBar: bottomNavigationBar,
+      bottomNavigationBar: BottomNavigation(screen: _screen),
       body: TripleBuilder(
         store: store.bottomBarStore,
         builder: (_, triple) {
